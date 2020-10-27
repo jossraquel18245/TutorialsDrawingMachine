fa609d249ed9244826eb980552ddc7d1368c4e51
jossraquel18245 <71808090+jossraquel18245@users.noreply.github.com>
2020-10-27 00:46:52 -0700

    CreatedNestedLoops

--- NestedLoops/sketch.js
+++ NestedLoops/sketch.js
@@ -3,41 +3,42 @@ let strokeWidth = 5;
 let array = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255, 134, 20);
+  createCanvas(600, 600);
+  //background(255, 134, 20);
 
 
+  drawGrid();
   noFill();
 }
 
 function draw() {
 
-background(255, 134, 20, 5);
-strokeWeight(strokeWidth);
+  background(255, 134, 20, 5);
+  strokeWeight(strokeWidth);
 
-noiseOffset += 0.05;
-strokeWidth = noise(noiseOffset) * 100;
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 100;
 
 
-if (mouseIsPressed){
-  line(mouseX, mouseY, pmouseX, pmouseY);
-  array.push([mouseX, mouseY]);
+  if (mouseIsPressed) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
 
-  stroke(map(mouseX, 0, 600, 0, 255, true));
-  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY );
-  line(mouseX, mouseY, pmouseX, pmouseY );
+    stroke(map(mouseX, 0, 600, 0, 255, true));
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+
 }
 
-}
-
-function keyTyped(){
+function keyTyped() {
 
   //console.log(`key ${key} is being typed`)
 
-  if (key === 's'){
+  if (key === 's') {
     // Save this image
     saveCanvas(`fileName`, 'png');
-  } else if (key === 'c'){
+  } else if (key === 'c') {
     //clear the image
     clear();
 
@@ -45,4 +46,24 @@ function keyTyped(){
   }
 
   return false;
+}
+
+function drawGrid() {
+  numCells = 20;
+  fillColor = 255;
+  //noStroke();
+  strokeWeight(0);
+
+  for (let i = 0; i <= width; i += width / numCells) {
+    for (let j = 0; j <= height; j += height / numCells) {
+      if (fillColor === 255) {
+        fillColor = 200;
+      } else {
+        fillColor = 255;
+      }
+      fill(fillColor);
+      rect(i, j, width / numCells, height / numCells);
+    }
+  }
+  strokeWeight(strokeWidth);
 }
